b3851f487f67ee90a97eb13d65979bab3a44b546
Koldobika Abasolo <koldobikaabasolo@gmail.com>
2025-03-17 18:24:54 +0100

    Yippieee

--- Classh.cpp
+++ Classh.cpp
@@ -1,16 +1,1 @@
-#include "Classs.hpp"
 
-Classs::Classs() {}
-
-Classs::Classs(const Classs& other) {*this = other;}
-
-Classs::~Classs() {}
-
-Classs&	Classs::operator = (const Classs& other)
-{
-	if (this != &other)
-	{
-
-	}
-	return (*this);
-}
